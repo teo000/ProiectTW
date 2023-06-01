89e4c5e536ad02b322e05a75ae26617d025110c7
Savin Miruna <miruna.savin02@gmail.com>
2023-06-01 22:55:40 +0300

    add sql script

---
+++ server/sql_scripts/script.sql
@@ -0,0 +1,11 @@
+--create table
+create table Users(
+                      ID serial primary key,
+                      Username varchar(255) not null,
+                      Email varchar(320),
+                      PasswordHash varchar(255) not null,
+                      Salt varchar(255) not null
+);
+
+--populate table
+
